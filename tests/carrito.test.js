44c61d3286f759ad2dabbc63ec2e8ee325d32f1a
Tuperfumeria <patricio.lafalce@gmail.com>
2025-08-06 11:48:35 -0300

    Migrar sitio estático de TuPerfumería

---
+++ tests/carrito.test.js
@@ -0,0 +1,6 @@
+// Ejemplo de test (a personalizar según funcionalidad)
+describe('Carrito de compras', () => {
+  it('agrega un producto al carrito', () => {
+    // tu lógica de test aquí
+  });
+});
